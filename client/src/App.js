c2ec7bef563b89f04ba8273e91d2675867806cf4
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-02-15 14:59:01 +0530

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
