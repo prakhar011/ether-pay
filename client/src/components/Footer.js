3638361b7d59a18f98b1265c2c77a59c556811b7
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-02-20 23:21:41 +0530

    add footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+
+import logo from "../images/logo.png";
 
 const Footer = () => {
+  const date = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} alt="logo" className="w-32" />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+        </div>
+      </div>
 
-export default Footer
+    
+
+      <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+
+      <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+        <p className="text-white text-left text-xs">@Prakhar Awasthi {date}</p>
+        <p className="text-white text-right text-xs">All rights reserved</p>
+      </div>
+    </div>)
+};
+
+export default Footer;
