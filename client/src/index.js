a2b1f05d591e4824afb60b6df64779f7dd075373
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-02-21 12:43:00 +0530

    add transaction provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { TransactionProvider } from "./context/TransactionContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <TransactionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionProvider>,
+  document.getElementById("root")
 );
