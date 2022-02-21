8c5063ceb5b3593a1d82cad17b2abec4386501e7
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-02-21 12:44:06 +0530

    transaction context

---
+++ client/src/context/TransactionContext.js
@@ -0,0 +1,20 @@
+import { useState, useEffect, createContext } from "react";
+import { ethers } from "ethers";
+import { contractABI, contractAddress } from "../utils/constants";
+export const TransactionContext = createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.provider.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+};
+
+export const TransactionProvider = ({ children }) => {
+  return <TransactionContext value={{}}>{children}</TransactionContext>;
+};
